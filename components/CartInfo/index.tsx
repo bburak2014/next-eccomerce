472e09571a4c7124dabb9a3f6803d348057f5b44
burak <burak.bilici@parayla.com>
2024-12-02 17:28:13 +0300

    product cart

--- components/CartInfo/index.tsx
+++ components/CartInfo/index.tsx
@@ -4,9 +4,11 @@ type Props = {}
 
 const index = (props: Props) => {
 	return (
-		<div className='w-full flex justify-between items-center pr-4 pl-10 py-7 border-top border-[#C1C1C1] border-0.5'>
-			<div className='flex'>
-				<div>Sipariş Özeti</div>
+		<div className='w-full flex justify-between items-center pr-4 pl-10 py-7 border-t-0.5 border-[#C1C1C1]  '>
+			<div className='flex gap-8'>
+				<div className='divide-x divide-slate-200'>Sipariş Özeti</div>
+				<div className="flex items-center">
+ </div>
 				<div>
 					<h3>Squier Bullet Strat Laurel Klavye Arctic Black Elektro Gitar</h3>
 					<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien sapien.</h3>
